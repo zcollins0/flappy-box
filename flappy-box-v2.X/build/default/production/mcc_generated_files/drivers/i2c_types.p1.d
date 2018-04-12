be70e7189cc78f5162ebfc5bb063ca5dd9e75054
Zach Collins <collinzy@mail.uc.edu>
2018-04-11 20:56:34 -0400

    remove V2

--- flappy-box-v2.X/build/default/production/mcc_generated_files/drivers/i2c_types.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/mcc_generated_files/drivers/i2c_types.d  \
- build/default/production/mcc_generated_files/drivers/i2c_types.p1:  \
- mcc_generated_files/drivers/i2c_types.c  \
-mcc_generated_files/drivers/i2c_types.h 
