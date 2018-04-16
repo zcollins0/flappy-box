f50f916dfea50983506cded4ea8631836821eb33
Zach Collins <collinzy@mail.uc.edu>
2018-04-16 15:07:42 -0400

    apply gitignore

--- flappy-box-v3.X/build/default/debug/mcc_generated_files/drivers/i2c_master.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/debug/mcc_generated_files/drivers/i2c_master.d  \
- build/default/debug/mcc_generated_files/drivers/i2c_master.p1:  \
- mcc_generated_files/drivers/i2c_master.c  \
-mcc_generated_files/drivers/../i2c_driver.h  \
-mcc_generated_files/drivers/i2c_types.h  \
-mcc_generated_files/drivers/i2c_master.h 
